2abff23629257c3f6f46b8d7b3a483efd0eb4342
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2015-08-20 16:56:42 +0200

    Allow build with CMake.

--- src/msx264.c
+++ src/msx264.c
@@ -22,6 +22,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "mediastreamer2/msvideo.h"
 #include "mediastreamer2/rfc3984.h"
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #ifdef _MSC_VER
 #include <stdint.h>
 #endif
